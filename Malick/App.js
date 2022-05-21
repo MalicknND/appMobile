41f42474b14354f0242df5ac1846253a649b2164
Malick NDIAYE <ndiayemalick062@gmail.com>
2022-05-21 17:48:29 +0200

    composants d'entrées

--- Malick/App.js
+++ Malick/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Text, Switch } from 'react-native';
 import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 
@@ -7,7 +7,28 @@ import MessagesScreen from './app/screens/MessagesScreen';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'T-shirt', value: 2 },
+  { label: 'Cameras', value: 3 },
+];
 
 export default function App() {
-  return <ListingsScreen />;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Catégorie"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
