b6d66113deea20d38b036d34ce65e0119f59b8ff
Malick NDIAYE <ndiayemalick062@gmail.com>
2022-05-22 17:13:20 +0200

    forms

--- Malick/App.js
+++ Malick/App.js
@@ -1,34 +1,7 @@
 import React, { useState } from 'react';
 import { View, TextInput, Text, Switch } from 'react-native';
-import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-
-import MessagesScreen from './app/screens/MessagesScreen';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-
-const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'T-shirt', value: 2 },
-  { label: 'Cameras', value: 3 },
-];
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Catégorie"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
