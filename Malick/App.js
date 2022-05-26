cf3e58eac45c427e81917e3783a7a0824f639a5e
Malick NDIAYE <ndiayemalick062@gmail.com>
2022-05-26 17:18:03 +0200

    forms

--- Malick/App.js
+++ Malick/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, Switch } from 'react-native';
+import { ListingEditScreen } from './app/screens/ListingEditScreen';
 import LoginScreen from './app/screens/LoginScreen';
+import RegisterScreen from './app/screens/RegisterScreen';
 
 export default function App() {
-  return <LoginScreen />;
+  return <ListingEditScreen />;
 }
