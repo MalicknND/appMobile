a5b97237d3e71ebbce0e468531b2b0c22d06aaa9
Malick NDIAYE <ndiayemalick062@gmail.com>
2022-05-27 08:04:40 +0200

    améliorations

--- Malick/App.js
+++ Malick/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import LoginScreen from './app/screens/LoginScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <MessagesScreen />;
 }
--- Malick/app/screens/MessagesScreen.js
+++ Malick/app/screens/MessagesScreen.js
@@ -3,15 +3,18 @@ import { FlatList, StyleSheet } from 'react-native';
 
 import Screen from '../components/Screen';
 
-import ListItem from '../components/ListItem';
-import ListItemSeparator from '../components/ListItemSeparator';
-import ListItemDeleteAction from '../components/ListItemDeleteAction';
+import {
+  ListItem,
+  ListItemDeleteAction,
+  ListItemSeparator,
+} from '../components/lists';
 
 const initialMessages = [
   {
     id: 1,
-    title: 'T1',
-    description: 'D1',
+    title: 'commentaire',
+    description:
+      "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).",
     image: require('../assets/malick.jpg'),
   },
   {
