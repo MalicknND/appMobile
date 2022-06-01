c1a768b35fa4733773fbe406c4fc15b4af687828
Malick NDIAYE <ndiayemalick062@gmail.com>
2022-06-01 21:00:47 +0200

    fonctionalité natives

--- Malick/App.js
+++ Malick/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
+
 import ListingEditScreen from './app/screens/ListingEditScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
 
 export default function App() {
   return <ListingEditScreen />;
